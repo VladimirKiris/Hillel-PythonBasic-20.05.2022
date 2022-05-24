5774532d16403b70b6697a33dd12909806373312
VladimirKiris <vladimirkiris@gmail.com>
2022-05-24 18:15:49 +0300

    homework edit

--- Homework2-24.05.2022.py
+++ Homework2-24.05.2022.py
@@ -1,6 +1,5 @@
 #kindergarden
 group_a = input('input group "a" capacity:  ')
 group_b = input('input group "b" capacity:  ')
-    #, input(int(group_a))
-beds = (int(group_a) // 2 + 1) + (int(group_b) // 2 + 1)
+beds = (int(group_a) // 2 + (int(group_a) % 2) + (int(group_b) // 2 ) + int(group_b) % 2)
 print('we will need ', beds, "beds")
