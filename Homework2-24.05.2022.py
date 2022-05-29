9194365885bd5888171b2a9014e22e9ae2e053a5
VladimirKiris <vladimirkiris@gmail.com>
2022-05-29 17:50:20 +0300

    Homework 4 added

--- Homework2-24.05.2022.py
+++ Homework2-24.05.2022.py
@@ -3,3 +3,4 @@ group_a = int(input('input group "a" capacity:  '))
 group_b = int(input('input group "b" capacity:  '))
 beds = (group_a // 2 + (group_a % 2) + (group_b // 2 ) + group_b % 2)
 print('we will need ', beds, "beds")
+#sdads
---
+++ Homework4-29.05.2022.py
@@ -0,0 +1,21 @@
+# Prime Numbers check
+# prime row first 25: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+import time
+a = int(input("Number check for Prime. \nInput positive int number: "))
+if a <= 1: exit("negative number, 0 or 1!!")
+start_time = time.time()
+for check in range(2, a):
+    if a % check == 0:
+        print("Your number ", a, " is composite")
+    break
+else:
+    print("\nYour number ", a, " is prime")
+print("Prime row before ", a, " is")
+for check in range(2, a):
+    for p_list in range(2, check):
+        if check % p_list == 0:
+            break
+    else:
+        print(check, end=", ")
+
+print("Executed in %s seconds " % (time.time() - start_time))
