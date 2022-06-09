111039e68e00459f7ed1d7b5340ad093fe1fc553
VladimirKiris <vladimirkiris@gmail.com>
2022-06-09 17:28:07 +0300

    Corrected range

--- Homework9-09.06.2022.py
+++ Homework9-09.06.2022.py
@@ -1,7 +1,7 @@
 #Search for Neighbour numbers in the random list, random range N, list length N/2
 import random
 n = input("Input random range, more than 0: ")
-lst = [random.randint(1, int(n)) for _ in range(round(int(n) / 2))]
+lst = [random.randint(1, int(n)) for _ in range((int(n) // 2))]
 print("List is: ", lst)
 s = input("Input number to search: ")
 ch = False
@@ -9,11 +9,11 @@ for i, el in enumerate(lst):
     if el == int(s):
         ch = True
         print("Found in psn", i)
-        if 0 < i < round(int(n) / 2) - 1:
+        if 0 < i < (int(n) // 2) - 1:
             print("Previous:", lst[i - 1], "Next:", lst[i + 1])
         elif i == 0:
             print("Next only available:", lst[i + 1])
-        elif i == round(int(n) / 2) - 1:
+        elif i == (int(n) // 2) - 1:
             print("Previous only available:", lst[i - 1])
 if not ch:
     print("No such value")
