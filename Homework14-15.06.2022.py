9a32c9861024fa1452ac22e3e75fb08b478f2836
VladimirKiris <vladimirkiris@gmail.com>
2022-06-15 20:02:50 +0300

    Add Homework 14

---
+++ Homework14-15.06.2022.py
@@ -0,0 +1,21 @@
+#Finding square root of x, 0 <= x <= (2 ** 31) - 1
+def sq_rt(x):
+    lst = list(range(0,x+1))
+    while True:
+        l = len(lst)
+        if len(lst) == 1:
+            lst = lst[0]
+            break
+        if lst[l // 2] ** 2 > x:
+            lst = lst[0 : l // 2]
+            l = len(lst)
+        elif lst[l // 2] ** 2 < x:
+            lst = lst[l // 2: l]
+            l = len(lst)
+        else:
+            lst = lst[len(lst) // 2]
+            break
+    return lst
+a = input("Input number to check square root: ")
+a = int(a)
+print("Square root of",a, "is", sq_rt(a))
