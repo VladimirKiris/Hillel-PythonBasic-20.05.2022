b438b0561d58517709fe14ffa364faeceb230162
VladimirKiris <vladimirkiris@gmail.com>
2022-06-15 20:16:38 +0300

    Corrected Homework 14

--- Homework14-15.06.2022.py
+++ Homework14-15.06.2022.py
@@ -3,15 +3,16 @@ def sq_rt(x):
     lst = list(range(0,x+1))
     while True:
         l = len(lst)
+        m = l // 2
         if l == 1:
             lst = lst[0]
             break
-        if lst[l // 2] ** 2 > x:
-            lst = lst[0 : l // 2]
-        elif lst[l // 2] ** 2 < x:
-            lst = lst[l // 2: l]
+        if lst[m] ** 2 > x:
+            lst = lst[0 : m]
+        elif lst[m] ** 2 < x:
+            lst = lst[m: l]
         else:
-            lst = lst[l // 2]
+            lst = lst[m]
             break
     return lst
 a = input("Input number to check square root: ")
