e43e195026a7d4dee6940634d7118e70c7ae3625
VladimirKiris <vladimirkiris@gmail.com>
2022-06-30 19:03:19 +0300

    Correct Homework 17

--- Homework17-28.06.2022.py
+++ Homework17-28.06.2022.py
@@ -3,8 +3,7 @@
 Returns maximum length of Game of cities sequence.
 
 cities = (
-    "Odessa Alchevsk Kremenchug Gostomel Lugansk Kirovograd Dnepr "
-    "Reni Izmail Lviv Poltava Avdeevka Arcyz Zaporozhie Enakievo Voznesensk Konotop"
+    "Odessa Alchevsk Kremenchug Gostomel Lugansk Kirovograd Dnepr Reni Izmail Lviv Poltava Avdeevka Arcyz Zaporozhie Enakievo Voznesensk Konotop"
 )
  """
 
@@ -13,17 +12,15 @@ def cities_game_rec(list_of_cities: list, city=None, cities_seq=None, start=None
     clone_cities = list_of_cities.copy()
     if city is not None:
         for i in clone_cities:
-            if str(cities_seq[-1])[-1] == i[0]:
+            if str(cities_seq[-1])[-1] == i[0] and i not in cities_seq:
                 cities_seq.append(i)
                 clone_cities.remove(i)
                 cities_game_rec(clone_cities, cities_seq[-1], cities_seq)
-                break
         return cities_seq
     else:
         cities_seq = list()
         city = clone_cities[start]
         clone_cities.remove(city)
-        cities_seq.clear()
         cities_seq.append(city)
         return cities_game_rec(clone_cities, city, cities_seq)
 
@@ -31,7 +28,6 @@ def cities_game_rec(list_of_cities: list, city=None, cities_seq=None, start=None
 cities = str(input("Please enter cities sequence, divided by space: "))
 cities_list = cities.lower()
 cities_list = cities_list.split()
-cities_list.sort(reverse=True)
 print("list is", cities_list)
 maximum = 0
 iteration = 0
